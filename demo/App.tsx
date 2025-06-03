356d88c87c6bf884ee38cd6cf15f097758578e34
nominalrune <59037577+nominalrune@users.noreply.github.com>
2025-06-03 22:45:27 +0900

    fix error

--- demo/App.tsx
+++ demo/App.tsx
@@ -32,7 +32,7 @@ function App() {
     <Markdown
       components={{
         code(props) {
-          const { children, className, node, ...rest } = props;
+          const { children, className, node, ref, ...rest } = props;
           const match = /language-(\w+)/.exec(className || '');
           return match ? (
             <Prism
